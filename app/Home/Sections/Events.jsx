f2882901da74dac12160835bfd5e70c4c676c2ea
OussamaERrafif <oussamaerra2002@gmail.com>
2024-01-17 13:48:23 +0100

    fixed events

--- app/Home/Sections/Events.jsx
+++ app/Home/Sections/Events.jsx
@@ -10,7 +10,7 @@ const Events = () => {
       <h1 className="text-2xl text-center font-bold mb-4 sm:mb-6 md:mb-8 lg:mb-10 xl:mb-12">
         Our <span className="bg-yellow-300">Events</span>:
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6 lg:gap-8 xl:gap-10">
+      <div className="lg:flex md:block sm:block gap-6 lg:gap-8 xl:gap-10">
         <Imgh src={project1} alt={"a1"} title={"Enactus Days"} />
         <Imgh src={project2} alt={"a1"} title={"Women Entrepreneur"} />
       </div>
--- app/Home/components/Imgh.jsx
+++ app/Home/components/Imgh.jsx
@@ -16,7 +16,7 @@ const Imgh = ({ src, alt, title }) => {
 
   return (
     <div
-      className='flex flex-col max-w-full sm:min-w-sm md:min-w-md lg:min-w-lg xl:min-w-xl rounded overflow-hidden shadow-lg transition-transform duration-500 ease-in-out relative m-4 '
+      className='flex flex-col max-w-full min-w-full  rounded overflow-hidden shadow-lg transition-transform duration-500 ease-in-out relative m-4 '
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
