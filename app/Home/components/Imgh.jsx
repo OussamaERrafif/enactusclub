73d57b127b8d88321b418b861473f38a36269209
OussamaERrafif <oussamaerra2002@gmail.com>
2024-01-17 14:31:21 +0100

    fixed events

--- app/Home/components/Imgh.jsx
+++ app/Home/components/Imgh.jsx
@@ -16,12 +16,12 @@ const Imgh = ({ src, alt, title }) => {
 
   return (
     <div
-      className='flex flex-col max-w-full min-w-28  rounded overflow-hidden shadow-lg transition-transform duration-500 ease-in-out relative m-4 '
+      className='flex flex-col max-w-full min-w-[200px] md:min-w-[300px] sm:min-w-[150px] rounded overflow-hidden shadow-lg transition-transform duration-500 ease-in-out relative m-4'
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <Image
-        className={`border-l-4 border-b-4 border-yellow-300 w-full h-full object-cover transition-transform duration-500 ease-in-out ${
+        className={`border-l-4 border-b-4 border-yellow-300 w-full h-full object-cover object-center transition-transform duration-500 ease-in-out ${
           isHovered ? 'scale-110 grayscale transition-filter' : 'reverse-grayscale'
         } z-0`}
         src={src}
@@ -29,7 +29,7 @@ const Imgh = ({ src, alt, title }) => {
       />
       {isHovered && (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 z-10">
-          <h1 className='text-white lg:text-3xl md:text-lg sm:text-lg font-bold m-4  border-4 p-2 border-yellow-300'>{title}</h1>
+          <h1 className='text-white lg:text-3xl md:text-lg sm:text-lg font-bold m-4 border-4 p-2 border-yellow-300'>{title}</h1>
           <Button className="m-4 sm:m-6 md:m-8 lg:m-10 xl:m-12" text={"Show More"} onClick={() => console.log("Button clicked")} />
         </div>
       )}
