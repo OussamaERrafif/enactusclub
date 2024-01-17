efe83c7e230587c5ed4ceb22528c0db19aa658f6
OussamaERrafif <oussamaerra2002@gmail.com>
2024-01-17 13:42:39 +0100

    fixed events

--- app/Home/components/Imgh.jsx
+++ app/Home/components/Imgh.jsx
@@ -16,7 +16,7 @@ const Imgh = ({ src, alt, title }) => {
 
   return (
     <div
-      className='flex flex-col max-w-full sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl rounded overflow-hidden shadow-lg transition-transform duration-500 ease-in-out relative m-4 sm:m-8 md:m-10 lg:m-12 xl:m-16'
+      className='flex flex-col max-w-full sm:min-w-sm md:min-w-md lg:min-w-lg xl:min-w-xl rounded overflow-hidden shadow-lg transition-transform duration-500 ease-in-out relative m-4 '
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
@@ -29,7 +29,7 @@ const Imgh = ({ src, alt, title }) => {
       />
       {isHovered && (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 z-10">
-          <h1 className='text-white text-3xl font-bold m-4 sm:m-6 md:m-8 lg:m-10 xl:m-12 border-4 p-2 border-yellow-300'>{title}</h1>
+          <h1 className='text-white lg:text-3xl md:text-lg sm:text-lg font-bold m-4  border-4 p-2 border-yellow-300'>{title}</h1>
           <Button className="m-4 sm:m-6 md:m-8 lg:m-10 xl:m-12" text={"Show More"} onClick={() => console.log("Button clicked")} />
         </div>
       )}
